701e7eedfb6204121862e874ea066aec27e7b842
Mellow <95871222+Mell-o@users.noreply.github.com>
2026-07-30 09:47:52 -0400

    Create data for article listing
    Created three articles. Added article images.

---
+++ data.js
@@ -0,0 +1,25 @@
+export const articles = [
+
+    {
+        articleDate: "July 14, 2002",
+        articleTitle: "Building projects and learning from mistakes",
+        articlePreview: 
+            "Each project presents new challenges, but debugging my code and solving problems has helped me become a more confident developer.",
+        articleImg: "./images/article-image-01.png",
+    },
+
+    {
+        articleTitle: "My experience working with JavaScript",
+        articleDate: "July 17, 2002",
+        articlePreview: 
+            "JavaScript has helped me make my projects more interactive. I am learning how to work with functions, arrays, objects, and the DOM.",
+        articleImg: "./images/article-image-02.png",
+    },
+
+    {
+        articleDate: "July 20, 2002",
+        articleTitle: "Learning to build responsive layouts",
+        articlePreview: "I practiced using Flexbox, CSS Grid, and media queries to create layouts that adapt smoothly to different screen sizes.",
+        articleImg: "./images/article-image-03.png",
+    }
+]
