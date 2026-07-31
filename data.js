53c5bac450b22107a39dfe1a19d517dc33285f41
Mellow <95871222+Mell-o@users.noreply.github.com>
2026-07-30 23:49:44 -0400

    Fix article dates and add linked content pages
    Fix date error for the article listings. Create the html files for the about and hero article. Properly link the nav links to the proper html file.

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const articleListings = [
 
     {
-        articleDate: "July 14, 2002",
+        articleDate: "July 14, 2022",
         articleTitle: "Building projects and learning from mistakes",
         articlePreview: 
             "Each project presents new challenges, but debugging my code and solving problems has helped me become a more confident developer.",
@@ -10,14 +10,14 @@ export const articleListings = [
 
     {
         articleTitle: "My experience working with JavaScript",
-        articleDate: "July 17, 2002",
+        articleDate: "July 17, 2022",
         articlePreview: 
             "JavaScript has helped me make my projects more interactive. I am learning how to work with functions, arrays, objects, and the DOM.",
         articleImg: "./images/article-image-02.png",
     },
 
     {
-        articleDate: "July 20, 2002",
+        articleDate: "July 20, 2022",
         articleTitle: "Learning to build responsive layouts",
         articlePreview: "I practiced using Flexbox, CSS Grid, and media queries to create layouts that adapt smoothly to different screen sizes.",
         articleImg: "./images/article-image-03.png",
