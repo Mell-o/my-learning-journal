7af0d5df093f0b6d9556c4736659373c77bc188b
Mellow <95871222+Mell-o@users.noreply.github.com>
2026-08-04 22:44:57 -0400

    Updated article listings data
    Added 3 more article listings.

--- data.js
+++ data.js
@@ -1,11 +1,10 @@
 export const articleListings = [
 
     {
-        articleDate: "July 14, 2022",
-        articleTitle: "Building projects and learning from mistakes",
-        articlePreview: 
-            "Each project presents new challenges, but debugging my code and solving problems has helped me become a more confident developer.",
-        articleImg: "./images/article-image-01.png",
+        articleDate: "July 20, 2022",
+        articleTitle: "Learning to build responsive layouts",
+        articlePreview: "I practiced using Flexbox, CSS Grid, and media queries to create layouts that adapt smoothly to different screen sizes.",
+        articleImg: "./images/article-image-03.png",
     },
 
     {
@@ -17,9 +16,34 @@ export const articleListings = [
     },
 
     {
-        articleDate: "July 20, 2022",
-        articleTitle: "Learning to build responsive layouts",
-        articlePreview: "I practiced using Flexbox, CSS Grid, and media queries to create layouts that adapt smoothly to different screen sizes.",
-        articleImg: "./images/article-image-03.png",
-    }
+        articleDate: "July 14, 2022",
+        articleTitle: "Building projects and learning from mistakes",
+        articlePreview: 
+            "Each project presents new challenges, but debugging my code and solving problems has helped me become a more confident developer.",
+        articleImg: "./images/article-image-01.png",
+    },
+
+    {
+        articleDate: "July 11, 2002",
+        articleTitle: "Getting more comfortable with Git and GitHub",
+        articlePreview:
+            "I practiced creating branches, writing clearer commit messages, and pushing my projects to GitHub so I can track my progress and share my work.",
+        articleImg: "./images/article-image-04.png",
+    },
+
+    {
+        articleDate: "July 8, 2002",
+        articleTitle: "Improving my CSS workflow",
+        articlePreview:
+            "I focused on organizing my styles, reusing classes, and paying closer attention to typography, spacing, and alignment throughout my projects.",
+        articleImg: "./images/article-image-05.png",
+    },
+
+    {
+        articleDate: "July 5, 2002",
+        articleTitle: "What I learned from code reviews",
+        articlePreview:
+            "Receiving feedback showed me how small changes can make my code cleaner, more accessible, and easier for other developers to understand.",
+        articleImg: "./images/article-image-06.png",
+    },
 ]
