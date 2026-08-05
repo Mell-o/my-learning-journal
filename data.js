d023c2bc07a37a1118b0643dfe9f55f76428badc
Mellow <95871222+Mell-o@users.noreply.github.com>
2026-08-05 11:28:30 -0400

    Fixed layout issues

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ export const articleListings = [
         articleDate: "July 20, 2022",
         articleTitle: "Learning to build responsive layouts",
         articlePreview: "I practiced using Flexbox, CSS Grid, and media queries to create layouts that adapt smoothly to different screen sizes.",
-        articleImg: "./images/article-image-03.png",
+        articleImg: "./images/article-image-01.png",
     },
 
     {
@@ -20,19 +20,19 @@ export const articleListings = [
         articleTitle: "Building projects and learning from mistakes",
         articlePreview: 
             "Each project presents new challenges, but debugging my code and solving problems has helped me become a more confident developer.",
-        articleImg: "./images/article-image-01.png",
+        articleImg: "./images/article-image-03.png",
     },
 
     {
-        articleDate: "July 11, 2002",
-        articleTitle: "Getting more comfortable with Git and GitHub",
+        articleDate: "July 11, 2022",
+        articleTitle: "Getting more comfortable with Git",
         articlePreview:
             "I practiced creating branches, writing clearer commit messages, and pushing my projects to GitHub so I can track my progress and share my work.",
         articleImg: "./images/article-image-04.png",
     },
 
     {
-        articleDate: "July 8, 2002",
+        articleDate: "July 8, 2022",
         articleTitle: "Improving my CSS workflow",
         articlePreview:
             "I focused on organizing my styles, reusing classes, and paying closer attention to typography, spacing, and alignment throughout my projects.",
@@ -40,7 +40,7 @@ export const articleListings = [
     },
 
     {
-        articleDate: "July 5, 2002",
+        articleDate: "July 5, 2022",
         articleTitle: "What I learned from code reviews",
         articlePreview:
             "Receiving feedback showed me how small changes can make my code cleaner, more accessible, and easier for other developers to understand.",
--- index.js
+++ index.js
@@ -10,13 +10,37 @@ const getFormattedDate = (articleDate) => {
     return `${year}-${month}-${day}`
 }
 
-articleListingsUl.innerHTML = articleListings.map(({articleDate, articleTitle, articlePreview, articleImg}) => {
-    return `
-    <article>
-        <img src="${articleImg}">
-        <time datetime="${getFormattedDate(articleDate)}">${articleDate}</time>
-        <h2>${articleTitle}</h2>
-        <p>${articlePreview}</p>
-    </article>
-    `
-}).join("")
+function buildArticleListingsUl (numOfListings) {
+
+    articleListingsUl.innerHTML = articleListings.slice(0, numOfListings).map(({articleDate, articleTitle, articlePreview, articleImg}) => {
+        return `
+        <li>
+            <article>
+                <img src="${articleImg}">
+                <time datetime="${getFormattedDate(articleDate)}">${articleDate}</time>
+                <h2>${articleTitle}</h2>
+                <p>${articlePreview}</p>
+            </article>
+        </li>
+        `
+    }).join("")
+}
+
+
+if (window.innerWidth < 1085) {
+    buildArticleListingsUl(3)
+} else if (window.innerWidth >= 1085 && document.querySelector(".home")) {
+    buildArticleListingsUl(articleListings.length)
+} else {
+    buildArticleListingsUl(3)
+}
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth < 1085) {
+        buildArticleListingsUl(3)
+    } else if (window.innerWidth >= 1085 && document.querySelector(".home")) {
+        buildArticleListingsUl(articleListings.length)
+    } else {
+        buildArticleListingsUl(3)
+    }
+});
